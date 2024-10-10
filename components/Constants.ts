bb45aedf5ee568eb2f872ad81e20113f9c203f57
Akshay Goyal <akshay.goyal1008@gmail.com>
2024-10-10 20:10:53 +0530

    minor changes

---
+++ components/Constants.ts
@@ -0,0 +1,62 @@
+export const people = [
+  {
+    image: "/image1.jpeg",
+    name: "Tom Smith",
+    story: [
+      "https://picsum.photos/450/800",
+      "https://exit109.com/~dnn/clips/RW20seconds_1.mp4#t=.1",
+    ],
+  },
+  {
+    image: "/image2.jpg",
+    name: "Helo Mask",
+    story: [
+      "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/1822458/pexels-photo-1822458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    image: "/image3.jpeg",
+    name: "Sweety Jha",
+    story: [
+      "https://images.pexels.com/photos/2106685/pexels-photo-2106685.jpeg",
+    ],
+  },
+  {
+    image: "/image4.jpeg",
+    name: "Amit Shina",
+    story: [
+      "https://images.pexels.com/photos/13325051/pexels-photo-13325051.jpeg",
+    ],
+  },
+  {
+    image: "/image5.jpeg",
+    name: "Anshul Agrawal",
+    story: [
+      "https://images.pexels.com/photos/1144686/pexels-photo-1144686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    image: "/image6.jpeg",
+    name: "Pari",
+    story: [
+      "https://images.pexels.com/photos/6032899/pexels-photo-6032899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    image: "/image7.jpeg",
+    name: "Ananya",
+    story: [
+      "https://images.pexels.com/photos/1028927/pexels-photo-1028927.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/1822458/pexels-photo-1822458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    image: "/image8.jpeg",
+    name: "Ankita Kaur",
+    story: [
+      " https://images.pexels.com/photos/1028927/pexels-photo-1028927.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/1822458/pexels-photo-1822458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+];
--- components/StorySlider.tsx
+++ components/StorySlider.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import gsap from "gsap";
 import styles from "../styles/StorySlider.module.css";
 import style from "../styles/NavBar.module.css";
 import Image from "next/image";
